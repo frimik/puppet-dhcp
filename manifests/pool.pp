7b66d62fead9c621bd0b2fe6d7c2c423beda32fa
Mikael Fridh <mfridh@marinsoftware.com>
2012-04-23 19:50:05 +0200

    dhcp::pool revert options stuff

--- manifests/pool.pp
+++ manifests/pool.pp
@@ -1,26 +1,18 @@
 define dhcp::pool (
-  $network,
-  $mask,
-  $range = undef,
-  $gateway,
-  $template = undef,
-  $options = undef
-) {
+    $network,
+    $mask,
+    $range,
+    $gateway
+  ) {
 
-  include dhcp::params
+    include dhcp::params
 
-  $dhcp_dir = $dhcp::dhcp_dir
+    $dhcp_dir = $dhcp::params::dhcp_dir
 
-  if !$template {
-    $real_template = $dhcp::params::pool_template
-  } else {
-    $real_template = $template
-  }
-
-  concat::fragment { "dhcp_pool_${name}":
-    target  => "${dhcp_dir}/dhcpd.pools",
-    content => template($real_template);
-  }
-
+    concat::fragment {
+        "dhcp_pool_${name}":
+            target  => "${dhcp_dir}/dhcpd.pools",
+            content => template("dhcp/dhcpd.pool.erb");
+    }
 }
 
